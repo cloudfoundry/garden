2a69f2e2e1949b7e1f78afa5527887fc2cbcb16a
Alex Suraci <suraci.alex@gmail.com>
2013-11-09 15:18:25 -0800

    add Linux backend

---
+++ backend/linux_backend/fake_container_pool/fake_container_pool.go
@@ -0,0 +1,46 @@
+package fake_container_pool
+
+import (
+	"github.com/vito/garden/backend"
+	"github.com/vito/garden/backend/fake_backend"
+)
+
+type FakeContainerPool struct {
+	CreateError  error
+	DestroyError error
+
+	ContainerSetup func(*fake_backend.FakeContainer)
+
+	CreatedContainers   []backend.Container
+	DestroyedContainers []backend.Container
+}
+
+func New() *FakeContainerPool {
+	return &FakeContainerPool{}
+}
+
+func (p *FakeContainerPool) Create(spec backend.ContainerSpec) (backend.Container, error) {
+	if p.CreateError != nil {
+		return nil, p.CreateError
+	}
+
+	container := fake_backend.NewFakeContainer(spec)
+
+	if p.ContainerSetup != nil {
+		p.ContainerSetup(container)
+	}
+
+	p.CreatedContainers = append(p.CreatedContainers, container)
+
+	return container, nil
+}
+
+func (p *FakeContainerPool) Destroy(container backend.Container) error {
+	if p.DestroyError != nil {
+		return p.DestroyError
+	}
+
+	p.DestroyedContainers = append(p.DestroyedContainers, container)
+
+	return nil
+}
