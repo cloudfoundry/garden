df053bee55993aedbc3853f7b2a571a49f05a708
Alex Suraci <suraci.alex@gmail.com>
2013-11-30 15:03:24 -0800

    detect filesystems via /proc/filesystems

--- backend/linux_backend/wshd/main_test.go
+++ backend/linux_backend/wshd/main_test.go
@@ -114,21 +114,6 @@ shopt -s nullglob
 
 rootfs_path=$1
 
-function get_distrib_codename() {
-  if [ -r /etc/lsb-release ]
-  then
-    source /etc/lsb-release
-
-    if [ -n "$DISTRIB_CODENAME" ]
-    then
-      echo $DISTRIB_CODENAME
-      return 0
-    fi
-  else
-    lsb_release -cs
-  fi
-}
-
 function overlay_directory_in_rootfs() {
   # Skip if exists
   if [ ! -d tmp/rootfs/$1 ]
@@ -145,49 +130,30 @@ function overlay_directory_in_rootfs() {
   mount -n --bind -o remount,$2 tmp/rootfs/$1 mnt/$1
 }
 
-function setup_fs_other() {
-  mkdir -p tmp/rootfs mnt
-  mkdir -p $rootfs_path/proc
-
-  mount -n --bind $rootfs_path mnt
-  mount -n --bind -o remount,ro $rootfs_path mnt
-
-  overlay_directory_in_rootfs /dev rw
-  overlay_directory_in_rootfs /etc rw
-  overlay_directory_in_rootfs /home rw
-  overlay_directory_in_rootfs /sbin rw
-  overlay_directory_in_rootfs /var rw
-
-  mkdir -p tmp/rootfs/tmp
-  chmod 777 tmp/rootfs/tmp
-  overlay_directory_in_rootfs /tmp rw
-}
-
-function setup_fs_ubuntu() {
-  mkdir -p tmp/rootfs mnt
-
-  distrib_codename=$(get_distrib_codename)
-
-  case "$distrib_codename" in
-  lucid|natty|oneiric)
-    mount -n -t aufs -o br:tmp/rootfs=rw:$rootfs_path=ro+wh none mnt
-    ;;
-  precise)
-    mount -n -t overlayfs -o rw,upperdir=tmp/rootfs,lowerdir=$rootfs_path none mnt
-    ;;
-  *)
-    echo "Unsupported: $distrib_codename"
-    exit 1
-    ;;
-  esac
-}
-
 function setup_fs() {
-  if grep -q -i ubuntu /etc/issue
+  mkdir -p tmp/rootfs mnt
+
+  if grep -q -i overlayfs /proc/filesystems
   then
-    setup_fs_ubuntu
+    mount -n -t overlayfs -o rw,upperdir=tmp/rootfs,lowerdir=$rootfs_path none mnt
+  elif grep -q -i aufs /proc/filesystems
+  then
+    mount -n -t aufs -o br:tmp/rootfs=rw:$rootfs_path=ro+wh none mnt
   else
-    setup_fs_other
+    mkdir -p $rootfs_path/proc
+
+    mount -n --bind $rootfs_path mnt
+    mount -n --bind -o remount,ro $rootfs_path mnt
+
+    overlay_directory_in_rootfs /dev rw
+    overlay_directory_in_rootfs /etc rw
+    overlay_directory_in_rootfs /home rw
+    overlay_directory_in_rootfs /sbin rw
+    overlay_directory_in_rootfs /var rw
+
+    mkdir -p tmp/rootfs/tmp
+    chmod 777 tmp/rootfs/tmp
+    overlay_directory_in_rootfs /tmp rw
   fi
 }
 
