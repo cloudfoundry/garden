581b069dfb971082b6131a2a08b9e501f4933d33
Alex Suraci <asuraci@pivotallabs.com>
2014-06-23 13:23:00 -0700

    fix fake client

--- client/fake_warden_client/fake_client.go
+++ client/fake_warden_client/fake_client.go
@@ -2,7 +2,6 @@ package fake_warden_client
 
 import (
 	"github.com/cloudfoundry-incubator/garden/client"
-	"github.com/cloudfoundry-incubator/garden/client/connection"
 	"github.com/cloudfoundry-incubator/garden/client/connection/fake_connection"
 	"github.com/cloudfoundry-incubator/garden/warden"
 )
@@ -13,22 +12,12 @@ type FakeClient struct {
 	Connection *fake_connection.FakeConnection
 }
 
-type FakeConnectionProvider struct {
-	Connection connection.Connection
-}
-
-func (provider *FakeConnectionProvider) ProvideConnection() (connection.Connection, error) {
-	return provider.Connection, nil
-}
-
 func New() *FakeClient {
 	connection := fake_connection.New()
 
 	return &FakeClient{
 		Connection: connection,
 
-		Client: client.New(&FakeConnectionProvider{
-			Connection: connection,
-		}),
+		Client: client.New(connection),
 	}
 }
