c3a939e5478c9cad238bdf7566cfd264bd8e925b
Craig Furman <cfurman@pivotal.io>
2017-07-27 12:10:30 +0100

    Increase timeout one bomberman test
    From graceTime + 50ms to graceTime * 2.
    
    [#149558185]

--- server/bomberman/bomberman_test.go
+++ server/bomberman/bomberman_test.go
@@ -29,7 +29,7 @@ var _ = Describe("Bomberman", func() {
 
 		select {
 		case <-detonated:
-		case <-time.After(backend.GraceTime(container) + 50*time.Millisecond):
+		case <-time.After(backend.GraceTime(container) * 2):
 			Fail("did not detonate!")
 		}
 	})
