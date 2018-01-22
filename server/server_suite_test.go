e0f4429387f46ea32bc58f5ee852959156fef27d
Tom Godkin <tgodkin@pivotal.io>
2018-01-22 12:28:16 +0000

    Increase listenAndServe timeout to 2s
    We *think* that tests fails sometimes because the server isn't quite
    ready.
    
    [#154516396]
    
    Signed-off-by: Georgi Sabev <georgethebeatle@gmail.com>

--- server/server_suite_test.go
+++ server/server_suite_test.go
@@ -1,13 +1,13 @@
 package server_test
 
 import (
+	"testing"
+
 	"code.cloudfoundry.org/garden/client"
 	"code.cloudfoundry.org/garden/client/connection"
 	"code.cloudfoundry.org/garden/server"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	"testing"
 )
 
 func TestServer(t *testing.T) {
@@ -16,12 +16,12 @@ func TestServer(t *testing.T) {
 }
 
 func listenAndServe(apiServer *server.GardenServer, network, addr string) {
-	Expect(apiServer.SetupBomberman()).To(Succeed())
+	ExpectWithOffset(1, apiServer.SetupBomberman()).To(Succeed())
 	go func() {
 		defer GinkgoRecover()
 		Expect(apiServer.ListenAndServe()).To(Succeed())
 	}()
 
 	apiClient := client.New(connection.New(network, addr))
-	Eventually(apiClient.Ping).Should(Succeed())
+	EventuallyWithOffset(1, apiClient.Ping, "2s").Should(Succeed())
 }
