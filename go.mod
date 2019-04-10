3885076ec916b21a71203d435e63abf121911028
Georgi Sabev <georgethebeatle@gmail.com>
2019-04-10 12:07:57 +0300

    Migrate to go modules
    [#164956617]
    
    Co-authored-by: Danail Branekov <danailster@gmail.com>

---
+++ go.mod
@@ -0,0 +1,11 @@
+module code.cloudfoundry.org/garden
+
+go 1.12
+
+require (
+	code.cloudfoundry.org/lager v2.0.0+incompatible
+	github.com/bmizerany/pat v0.0.0-20170815010413-6226ea591a40 // indirect
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.5.0
+	github.com/tedsuo/rata v1.0.0
+)
--- scripts/update-godoc.sh
+++ scripts/update-godoc.sh
@@ -2,21 +2,4 @@
 set -e
 set -x
 
-repoPath=$(cd $(dirname $BASH_SOURCE)/.. && pwd)
-
-if [ -z $GOROOT ]; then
-  export GOROOT=/usr/local/go
-  export PATH=$GOROOT/bin:$PATH
-fi
-
-if [ -z $GOPATH ]; then
-  export GOPATH=$HOME/go
-  export PATH=$GOPATH/bin:$PATH
-fi
-
-cd $(dirname $0)/..
-mkdir -p $GOPATH/src/code.cloudfoundry.org
-ln -s $PWD $GOPATH/src/code.cloudfoundry.org/garden
-
-cd $GOPATH/src/code.cloudfoundry.org/garden
-go run scripts/update-godoc/main.go
+go run garden/scripts/update-godoc/main.go
